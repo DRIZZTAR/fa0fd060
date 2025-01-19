ce81bf50442af5948dc370d6880af80630d2c946
DRIZZTAR <tyson_jeremy@hotmail.com>
2025-01-18 22:00:20 -0700

    fix padding for bottom nav mobile

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,7 +9,7 @@ function Layout({
 	setActiveTab,
 }) {
 	return (
-		<main className='min-h-screen bg-white flex items-center justify-center py-10'>
+		<main className='min-h-screen bg-white flex items-center justify-center pb-14'>
 			<div className='bg-[#E9E9E9] rounded-lg shadow-xl overflow-hidden max-h-[812px] h-[90vh] w-full max-w-[375px] flex flex-col'>
 				<Header
 					inboxCount={inboxCount}
