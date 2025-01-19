9a3b55fa6a24692b3088adc4b3839f42874c3a79
DRIZZTAR <tyson_jeremy@hotmail.com>
2025-01-18 21:58:32 -0700

    remove custom vh css, test mobile

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,7 +9,7 @@ function Layout({
 	setActiveTab,
 }) {
 	return (
-		<main className='min-h-[calc(var(--vh)*100)] bg-white flex items-center justify-center pb-10'>
+		<main className='min-h-screen bg-white flex items-center justify-center py-10'>
 			<div className='bg-[#E9E9E9] rounded-lg shadow-xl overflow-hidden max-h-[812px] h-[90vh] w-full max-w-[375px] flex flex-col'>
 				<Header
 					inboxCount={inboxCount}
