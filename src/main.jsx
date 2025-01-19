0597fad1e83eecd505eefcdf52740f424afd2fba
DRIZZTAR <tyson_jeremy@hotmail.com>
2025-01-18 21:52:24 -0700

    add event listener for mobile responsive screens to account for chrome, and safari bars

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Function to set the --vh property
+function setVh() {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+setVh();
+
+window.addEventListener('resize', setVh);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
